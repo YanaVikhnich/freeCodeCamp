c226766e72e06c68fa50fd6650be9a4a70271870
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-17 19:08:03 +0300

    Changes in server adding return packages

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
+
+app.get('/package', (req, res) => {
+    const packagePath = path.join(__dirname, 'package.json');
+    fs.readFile(packagePath, 'utf8', (err, data) => {
+        if (err) return res.status(500).send('Error reading package.json');
+        res.type('application/json').send(data);
+    });
+});
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
