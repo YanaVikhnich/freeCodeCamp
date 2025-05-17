6b8ec716ba959034c3ddad22695aa631f8f2e359
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-17 18:47:44 +0300

    Changes in server

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
-    console.log(`Server is running om http://localhost:${PORT}`);
-    
+    console.log(`Server running on port ${PORT}`);
 });
