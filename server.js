548c2dff0392011c1b1a049dc66d7ec712897c5f
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-17 20:55:14 +0300

    Adding localhost

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
