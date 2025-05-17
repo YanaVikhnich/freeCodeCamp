b11de4c0cff3971c87d2f01c3c9e3bf046560932
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-17 19:50:06 +0300

    Changes in server file

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/package.json', (req, res) => {
   res.sendFile(path.join(__dirname, 'package.json')); // <-- повертаємо JSON
 });
 
+app.use(express.Router.static(__dirname));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
