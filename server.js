395e4001e087041e8c1c88927dfea6e6dd34dd89
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-17 19:24:55 +0300

    Requires to packagejson

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
 const app = express();
+const packageJson = require('./package.json');
 
 app.get('/package', (req, res) => {
-    const packagePath = path.join(__dirname, 'package.json');
-    fs.readFile(packagePath, 'utf8', (err, data) => {
-        if (err) return res.status(500).send('Error reading package.json');
-        res.type('application/json').send(data);
-    });
+    res.json(packageJson);
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
