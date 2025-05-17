26474c110666fc787d2b31f88c16c0fee4ab364a
Yana <93792226+YanaVikhnich@users.noreply.github.com>
2025-05-17 19:28:20 +0300

    Server changes

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const packageJson = require('./package.json');
+const packageJson = require('./package.json'); // <-- імпортуємо напряму
 
-app.get('/package', (req, res) => {
-    res.json(packageJson);
+app.get('/package.json', (req, res) => {
+  res.json(packageJson); // <-- повертаємо JSON
 });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
